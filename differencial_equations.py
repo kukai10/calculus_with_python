b5b9b1a7d689727cfaa77ab56d02f599123dcd66
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-15 15:24:21 -0400

    Update differencial_equations.py

--- differencial_equations.py
+++ differencial_equations.py
@@ -5,3 +5,32 @@ def solve_differential_equation(equations):
 
 def solve_differential_equation_matrix_form(matrix_A):
     pass
+
+
+def Eulers_method_for_DE(initial_value, t0, h, derivative_function_respect_to_time):
+    """
+    euler's method to approximate first order differential equation
+    conditions to use this function:
+        1.) the function must behave well (continuous, linear)
+        2.) y' = F(x, y)
+        3.) initial value and the slope at that point is known
+
+    Explanation:
+        initial_value is the starting point
+        t0 is the inital time
+        h is the step interval, approximation gets better as h gets smaller
+
+
+
+    """
+    #dydt_at_t0 = derivative_function_respect_to_time(t0, initial_value)
+    pass
+
+def sketch_direction_fields(equation):
+    pass
+
+
+def fourier_series(equation):
+    pass
+
+
