0fe27241f109cf36351d9ddc6628e2b5d59b1e20
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-15 12:00:52 -0400

    Create intermediate_math_functions.py

---
+++ intermediate_math_functions.py
@@ -0,0 +1,8 @@
+import math
+
+def perpendicular_slope(slope, equation=None, point=None, derivative_equation=None):
+    if equation == point == derivative_equation == None:
+        return -1/slope
+    else:
+        pass
+        # find slope at a point given an equation or the derivative of the equation
